1a959dce362f79079f560d3d76efe51e19642d55
Kevin Steinke <kjsteinke@gmail.com>
2024-04-02 17:48:21 -0700

    Update gtag.js

--- _includes/gtag.js
+++ _includes/gtag.js
@@ -1,4 +1,4 @@
-!-- Google tag (gtag.js) -->
+<!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-WEENMS9FCD"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
