49c0cc9d68f7808cf11c5961988fc320c3084e63
Kevin Steinke <kjsteinke@gmail.com>
2024-04-02 21:08:32 -0700

    Update gtag.js

--- _includes/gtag.js
+++ _includes/gtag.js
@@ -7,12 +7,4 @@
 
   gtag('config', 'G-WEENMS9FCD');
 </script>
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-QS2W5EXD71"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-QS2W5EXD71');
-</script>
