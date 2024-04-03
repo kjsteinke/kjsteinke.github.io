14fb4940a93e4a54f6a5ea40bf18c63c5cb35a2d
Kevin Steinke <kjsteinke@gmail.com>
2024-04-02 17:54:09 -0700

    Update gtag.js

--- _includes/gtag.js
+++ _includes/gtag.js
@@ -7,3 +7,12 @@
 
   gtag('config', 'G-WEENMS9FCD');
 </script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-QS2W5EXD71"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QS2W5EXD71');
+</script>
